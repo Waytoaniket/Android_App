237c78deb1c600958ada3a5a310cf420de39b97e
Harshita <h2010singh@gmail.com>
2019-12-05 15:55:06 +0530

    Progress Bar

--- app/src/main/java/com/example/template/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/template/ui/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.example.template.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -23,6 +25,9 @@ class HomeFragment : Fragment() {
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
+        val progressBar:ProgressBar = root.findViewById(R.id.progressBar2)
+        progressBar.secondaryProgress = 50
+        Log.d("Hello","secondary progress set")
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
