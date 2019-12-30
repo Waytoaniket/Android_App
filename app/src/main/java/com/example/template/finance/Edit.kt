2de79092853867044588cc8f6e2d330cb19f0a35
waytoaniket <waytoaniketgupta@gmail.com>
2019-12-30 17:04:10 +0530

    Small changes in styles.xml

--- app/src/main/java/com/example/template/finance/Edit.kt
+++ app/src/main/java/com/example/template/finance/Edit.kt
@@ -2,7 +2,9 @@ package com.example.template.finance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.template.R
+import kotlinx.android.synthetic.main.activity_edit.*
 
 class Edit : AppCompatActivity() {
 
@@ -10,5 +12,10 @@ class Edit : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
         supportActionBar?.title="Edit"
+        val Family_info=intent.getStringArrayListExtra("Family_info")
+        Log.d("Family111122",Family_info.toString())
+        Existing_family_Id.text=Family_info[0]
+        Username.setText(Family_info[1])
+        Amount.text=Family_info[2]
     }
 }
